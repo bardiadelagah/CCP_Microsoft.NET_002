4810065caf886dffc3cf43ba47910ed6d14c58ad
Your Name <bdelagah@gmail.com>
2023-12-03 02:12:59 +0330

    first commit

---
+++ Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,29 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories;
+
+[BindProperties]
+public class CreateModel : PageModel
+{
+    private readonly ApplicationDbContext _db;
+    //[BindProperty]
+    public Category Category {get; set;}
+    public CreateModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public void OnGet()
+    {
+    }
+
+    public IActionResult OnPost() {//Category obj
+        _db.Categories.Add(Category);//obj
+        _db.SaveChanges();
+        TempData["success"] =  "Category Create successfully";
+        return RedirectToPage("Index");
+    }
+}
